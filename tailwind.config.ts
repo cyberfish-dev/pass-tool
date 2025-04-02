eb3d7b33616d8f9e2cd3e62583d01314017ca267
cyberfish <sudakovmail@gmail.com>
2025-04-02 17:18:00 +0900

    fixing issue with css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   darkMode: ['class', 'class'],
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', , './pageParts/**/*.{js,ts,jsx,tsx}'],
   theme: {
   	extend: {
   		borderRadius: {
