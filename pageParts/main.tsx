7b1b783d31a3f98a6f84f26aadafb1d1d68f7a4a
cyberfish <sudakovmail@gmail.com>
2025-04-07 22:37:49 +0900

    privacy terms and chose default

---
+++ pageParts/main.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import GeneratorView from "../pageParts/generator";
+import VaultView from "../pageParts/vault";
+import Header from "../pageParts/header";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Footer from "@/pageParts/footer";
+
+export default function MainPage() {
+
+
+    const [defaultValue, setDefaultValue] = useState(() => {
+        const saved = localStorage.getItem("defValue");
+        return saved ?? 'generator';
+    });
+
+    const handleTabChange = (newTab: string) => {
+        localStorage.setItem("defValue", newTab);
+    };
+
+    return (
+        <main className="min-h-screen p-4 md:p-8 space-y-6">
+            <div className="max-w-xl mx-auto space-y-6">
+
+                <Header />
+
+                <Tabs defaultValue={defaultValue} onValueChange={handleTabChange} className="w-full">
+
+                    <TabsList className="mb-2">
+                        <TabsTrigger value="generator">Password Generator</TabsTrigger>
+                        <TabsTrigger value="vault">Password Vault</TabsTrigger>
+                    </TabsList>
+
+                    <TabsContent value="generator">
+
+                        <GeneratorView></GeneratorView>
+
+                    </TabsContent>
+
+                    <TabsContent value="vault">
+                        <VaultView></VaultView>
+                    </TabsContent>
+                </Tabs>
+
+                <Footer />
+
+            </div></main>)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react";
-import GeneratorView from "../pageParts/generator";
-import VaultView from "../pageParts/vault";
-import Header from "../pageParts/header";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import MainPage from "@/pageParts/main";
 
 if (typeof window !== "undefined" && "serviceWorker" in navigator) {
   window.addEventListener("load", () => {
@@ -24,29 +21,5 @@ export default function App() {
     return (<></>);
   }
 
-  return (
-    <main className="min-h-screen p-4 md:p-8 space-y-6">
-      <div className="max-w-xl mx-auto space-y-6">
-
-        <Header />
-
-        <Tabs defaultValue="generator" className="w-full">
-
-          <TabsList className="mb-2">
-            <TabsTrigger value="generator">Password Generator</TabsTrigger>
-            <TabsTrigger value="vault">Password Vault</TabsTrigger>
-          </TabsList>
-
-          <TabsContent value="generator">
-
-            <GeneratorView></GeneratorView>
-
-          </TabsContent>
-
-          <TabsContent value="vault">
-            <VaultView></VaultView>
-          </TabsContent>
-        </Tabs>
-
-      </div></main>)
+  return (<MainPage></MainPage>)
 }
