3caa149147be28765f37763585adc2c8421bf0b0
cyberfish <sudakovmail@gmail.com>
2025-06-21 02:57:07 +0800

    more styling

--- flutter_app/lib/themes/colors.dart
+++ flutter_app/lib/themes/colors.dart
@@ -5,12 +5,13 @@ const darkBg = Color(0xFF22272C);
 const darkMenu = Color(0xFF2D343C);
 const darkCard = Color(0xFF2D343C
 );
-const accent = Color(0xFF08bdbc);
+const accent = Color(0xFF9EFFF7);
 
 final textSecondary = Color(0xffC9D4E9).withAlpha(210);
-final dividerColor = textSecondary.withAlpha(15);
-final inputBorderColor = textSecondary.withAlpha(35);
+final dividerColor = textSecondary.withAlpha(10);
+final inputBorderColor = textSecondary.withAlpha(10);
+
 final textPrimary = Color(0xFFFCFCFC).withAlpha(240);
-final iconsColor = accent.withAlpha(180);
+final iconsColor = Color(0xFF5A6068);
 
 const errorColor = Color(0xffFFBF00);
